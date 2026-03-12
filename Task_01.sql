1fedce30e4e0523be184be73f20086dda29643a6
ImrenR <imrenrahbay@gmail.com>
2026-03-12 11:45:58 +0000

    up

--- Task_01.sql
+++ Task_01.sql
@@ -1,114 +1,108 @@
-create table  if not exists hastaneler( -- eger yoksa hastaneler isminde tablo tanimla
-id char(5) primary key,
-isim varchar(50),
-sehir varchar(10),
-ozel char(1) -- ozel boolean default true
+USE javacan;
 
+CREATE TABLE IF NOT EXISTS hospitals(  -- create a table named hospitals if it doesn't exist
+    id CHAR(5) PRIMARY KEY,
+    name VARCHAR(50),
+    city VARCHAR(10),
+    private CHAR(1) -- private boolean, default true
 );
-insert into hastaneler values('H101','ACI MADEM HASTANESI','ISTANBUL','Y');
-insert into hastaneler values('H102','HASZEKI HASTANESI','ISTANBUL','N');
-insert into hastaneler values('H103','MEDIKOL HASTANESI','IZMIR','Y');
-insert into hastaneler values('H104','NEMORYIL HASTANESI','ANKARA','Y');
 
-    CREATE TABLE bolumler
-    (
-        bolum_id CHAR(5) PRIMARY KEY,
-        bolum_adi VARCHAR(20)
-    ); 
-    
-    INSERT INTO bolumler VALUES('DHL', 'Dahiliye');
-    INSERT INTO bolumler VALUES('KBB', 'Kulak-Burun-Boğaz');
-    INSERT INTO bolumler VALUES('NRJ', 'Noroloji');
-    INSERT INTO bolumler VALUES('GAST', 'Gastoroloji');
-    INSERT INTO bolumler VALUES('KARD', 'Kardioloji');
-    INSERT INTO bolumler VALUES('PSK', 'Psikiyatri');
-    INSERT INTO bolumler VALUES('GOZ', 'Goz Hastaliklari');
-     
-    CREATE TABLE hastalar
-    (
-        kimlik_no CHAR(11) PRIMARY KEY,
-        isim VARCHAR(20),
-        teshis VARCHAR(30)
-    );
-    
-    INSERT INTO hastalar VALUES('12345678901', 'Ali Can','Gizli Seker');
-    INSERT INTO hastalar VALUES('45678901121', 'Ayşe Yılmaz','Hipertansiyon');
-    INSERT INTO hastalar VALUES('78901123451', 'Steve Job','Pankreatit');
-    INSERT INTO hastalar VALUES('12344321251', 'Tom Hanks','COVID19');
-    
-    CREATE TABLE hasta_kayitlar
-    (
-        kimlik_no CHAR(11) primary key,
-        hasta_isim VARCHAR(20),
-        hastane_adi VARCHAR(50),
-        bolum_adi VARCHAR(20), 
-        teshis VARCHAR(30)
-    ); 
-    
-    INSERT INTO hasta_kayitlar VALUES('1111','NONAME','','','');
-    INSERT INTO hasta_kayitlar VALUES('2222','NONAME','','','');
-    INSERT INTO hasta_kayitlar VALUES('3333','NONAME','','','');
-    INSERT INTO hasta_kayitlar VALUES('4444','NONAME','','','');
-    INSERT INTO hasta_kayitlar VALUES('5555','NONAME','','','');
-    
-select * from hastaneler;
-select * from bolumler;
-select * from hastalar;
-select * from hasta_kayitlar;
+INSERT INTO hospitals VALUES('H101','ACI MADEM HOSPITAL','ISTANBUL','Y');
+INSERT INTO hospitals VALUES('H102','HASZEKI HOSPITAL','ISTANBUL','N');
+INSERT INTO hospitals VALUES('H103','MEDIKOL HOSPITAL','IZMIR','Y');
+INSERT INTO hospitals VALUES('H104','NEMORYIL HOSPITAL','ANKARA','Y');
 
-/*
+CREATE TABLE departments
+(
+    dept_id CHAR(5) PRIMARY KEY,
+    dept_name VARCHAR(20)
+); 
 
- hasta_kayitlar tablosundaki ‘3333’ kimlik nolu kaydı aşağıdaki gibi güncelleyiniz. 
- hasta_isim : ‘Salvadore Dali’ ismi ile
-hastane_adi: ‘John Hopins’
-bolum_adi: ‘Noroloji’
-teshis: ‘Parkinson’
-kimlik_no: ‘99991111222’
-*/
+INSERT INTO departments VALUES('DHL', 'Internal Medicine');
+INSERT INTO departments VALUES('KBB', 'Ear-Nose-Throat');
+INSERT INTO departments VALUES('NRJ', 'Neurology');
+INSERT INTO departments VALUES('GAST', 'Gastroenterology');
+INSERT INTO departments VALUES('KARD', 'Cardiology');
+INSERT INTO departments VALUES('PSK', 'Psychiatry');
+INSERT INTO departments VALUES('GOZ', 'Eye Diseases');
 
-update hasta_kayitlar
-set
-hasta_isim='Salvadore Dali',
-hastane_adi='John Hopkins',
-bolum_adi='noroloji',
-teshis='Parkinson',
-kimlik_no='99991111222'
-where kimlik_no='3333';
+CREATE TABLE patients
+(
+    national_id CHAR(11) PRIMARY KEY,
+    name VARCHAR(20),
+    diagnosis VARCHAR(30)
+);
 
-/*
-hasta_kayıtlar tablosundaki ‘1111’ kimlik nolu kaydı aşağıdaki gibi güncelleyiniz.
-hasta_isim : hastalar tablosundaki ‘Ali Can’ ismi ile
-hastane_adi: hastaneler tablosundaki 'H104' bolum_id kodlu hastanenin ismi ile 
-bolum_adi: bolumler tablosundaki 'DHL' bolum_id kodlu bolum_adi ile
-teshis: hastalar tablosundaki 'Ali Can' isimli hastanın teshis bilgisi ile
-kimlik_no: hastalar tablosundaki 'Ali Can' isimli hastanın kimlik_no kodu ile
+INSERT INTO patients VALUES('12345678901', 'Ali Can','Diabetes');
+INSERT INTO patients VALUES('45678901121', 'Ayşe Yılmaz','Hypertension');
+INSERT INTO patients VALUES('78901123451', 'Steve Job','Pancreatitis');
+INSERT INTO patients VALUES('12344321251', 'Tom Hanks','COVID19');
 
-*/
+CREATE TABLE patient_records
+(
+    national_id CHAR(11) PRIMARY KEY,
+    patient_name VARCHAR(20),
+    hospital_name VARCHAR(50),
+    department_name VARCHAR(20), 
+    diagnosis VARCHAR(30)
+); 
 
-update hasta_kayitlar
-set
-hasta_isim=(select isim from hastalar where isim='Ali Can'),
-hastane_adi=(select isim from hastaneler where id='H104'),
-bolum_adi=(select bolum_adi from bolumler where bolum_id='DHL'),
-teshis=(select teshis from hastalar where isim='Ali Can'),
-kimlik_no=(select kimlik_no from hastalar where isim='Ali Can')
-where kimlik_no='1111';
-select * from hasta_kayitlar;
+INSERT INTO patient_records VALUES('1111','NONAME','','','');
+INSERT INTO patient_records VALUES('2222','NONAME','','','');
+INSERT INTO patient_records VALUES('3333','NONAME','','','');
+INSERT INTO patient_records VALUES('4444','NONAME','','','');
+INSERT INTO patient_records VALUES('5555','NONAME','','','');
 
-/*
-hasta_kayıtlar tablosundaki ‘2222’ kimlik nolu kaydı aşağıdaki gibi güncelleyiniz. 
-hasta_isim : hastalar tablosundaki ‘Ayşe Yılmaz’ ismi ile
-hastane_adi: hastaneler tablosundaki 'H103' bolum_id kodlu hastanenin ismi ile 
-bolum_adi: bolumler tablosundaki ‘KBB’ bolum_id kodlu bolum_adi ile
-teshis: hastalar tablosundaki ‘Tom Hanks' isimli hastanın teshis bilgisi ile 
-kimlik_no: hastalar tablosundaki 'Stevev Job' isimli hastanın kimlik_no kodu ile
-*/
-update hasta_kayitlar
-set
-hasta_isim=(select isim from hastalar where isim='ayşe Yılmaz'),
-hastane_adi=(select isim from hastaneler where id='H103'),
-bolum_adi=(select bolum_adi from bolumler where bolum_id='KBB'),
-teshis=(select teshis from hastalar where isim='Tom Hanks'),
-kimlik_no=(select kimlik_no from hastalar where isim='Steve Job')
-where kimlik_no='2222';
-select * from hasta_kayitlar;
+SELECT * FROM hospitals;
+SELECT * FROM departments;
+SELECT * FROM patients;
+SELECT * FROM patient_records;
+
+--[1] Update the record in patient_records with national_id '3333'
+-- Set the fields as follows:
+-- patient_name: 'Salvadore Dali'
+-- hospital_name: 'John Hopins'
+-- department_name: 'Neurology'
+-- diagnosis: 'Parkinson'
+-- national_id: '99991111222'
+UPDATE patient_records
+SET patient_name = 'Salvadore Dali',
+    hospital_name='John Hopins',
+    department_name='Neurology',
+    diagnosis='Parkinson',
+    national_id='99991111222'
+WHERE national_id='3333';
+
+--[2] Update the record in patient_records with national_id '1111'
+-- Set the fields as follows:
+-- patient_name: the name of 'Ali Can' from the patients table
+-- hospital_name: the name of the hospital with id 'H104' from the hospitals table
+-- department_name: the department_name of dept_id 'DHL' from the departments table
+-- diagnosis: the diagnosis of 'Ali Can' from the patients table
+-- national_id: the national_id of 'Ali Can' from the patients table
+UPDATE patient_records
+SET
+    patient_name = (SELECT name FROM patients WHERE name='Ali Can'),
+    hospital_name = (SELECT name FROM hospitals WHERE id='H104'),
+    department_name = (SELECT dept_name FROM departments WHERE dept_id='DHL'),
+    diagnosis = (SELECT diagnosis FROM patients WHERE name='Ali Can'),
+    national_id = (SELECT national_id FROM patients WHERE name='Ali Can')
+WHERE national_id='1111';
+SELECT * FROM patient_records;
+
+--[3] Update the record in patient_records with national_id '2222'
+-- Set the fields as follows:
+-- patient_name: the name of 'Ayşe Yılmaz' from the patients table
+-- hospital_name: the name of the hospital with id 'H103' from the hospitals table
+-- department_name: the department_name of dept_id 'KBB' from the departments table
+-- diagnosis: the diagnosis of 'Tom Hanks' from the patients table
+-- national_id: the national_id of 'Steve Job' from the patients table
+UPDATE patient_records
+SET
+    patient_name = (SELECT name FROM patients WHERE name='Ayşe Yılmaz'),
+    hospital_name = (SELECT name FROM hospitals WHERE id='H103'),
+    department_name = (SELECT dept_name FROM departments WHERE dept_id='KBB'),
+    diagnosis = (SELECT diagnosis FROM patients WHERE name='Tom Hanks'),
+    national_id = (SELECT national_id FROM patients WHERE name='Steve Job')
+WHERE national_id='2222';
+SELECT * FROM patient_records;
